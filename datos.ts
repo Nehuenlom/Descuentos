20fb2699c3c516e4519de76e79365bba6559240a
Nehuenlom <93491147+Nehuenlom@users.noreply.github.com>
2021-11-08 17:45:45 -0300

    Initial commit

---
+++ datos.ts
@@ -0,0 +1,13 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let dato2 = document.getElementById("dato2");
+
+btnEnv.addEventListener("click", () => {
+  // TU CODIGO VA AQUI, POR EJEMPLO:
+  console.log("el dato ingresado es ", dato.value);
+  console.log("el dato ingresado es ", dato2.value);
+  let resultado: number = Number(dato.value) * Number(dato2.value);
+  console.log("el area del rectangulo es ");
+  console.log(resultado);
+  //}
+});
